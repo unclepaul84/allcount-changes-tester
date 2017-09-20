1eb6e49112d88b94adfea3748db1a4a5a4536134
Paul Kotlyar <unclepaul84@gmail.com>
2017-09-20 15:38:12 -0500

    auzure event hub crud publishing working

--- server.js
+++ server.js
@@ -7,16 +7,30 @@ process.env['NODE_ENV'] = 'development';
 
 injection.bindFactory('defaultBaseUrl', 'http://localhost:' + port);
 
-if(process.env.BASE_URL)
-    injection.bindFactory('defaultBaseUrl', process.env.BASE_URL);
 
 injection.bindFactory('port', port);
 injection.bindFactory('dbUrl', 'mongodb://pkallcounttester:UrhmV7OgZrZwNMhm7vGwtG7l9ipRYsPpERgXqCekKo7KfuTQXgvslnm9WU7pi9M8LaejjITNPL4ZWGOZjjZYdg==@pkallcounttester.documents.azure.com:10255/allcount?ssl=true&replicaSet=globaldb');
 injection.bindFactory('gitRepoUrl', 'https://gist.github.com/90492c822dc2d7ed319cdcad9d255de3.git');
 
+
+
+if (process.env.CONFIG_GIT_URL)
+    injection.bindFactory('gitRepoUrl', process.env.CONFIG_GIT_URL);
+
+if (process.env.MONGO_DB_URL)
+    injection.bindFactory('dbUrl', process.env.MONGO_DB_URL);
+
+if (process.env.BASE_URL)
+    injection.bindFactory('defaultBaseUrl', process.env.BASE_URL);
+
+
 const adalmodule = require("./adal-auth-module/adal-auth-module.js");
 
 adalmodule.installModule(injection);
+
+const eventHubModule = require("./azure-event-hubs-module/azure-event-hubs-module.js");
+
+eventHubModule.installModule(injection);
 
 var server = injection.inject('allcountServerStartup');
 
