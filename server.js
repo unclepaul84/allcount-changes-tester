376aadbfc1301d69e0dc4f1ecf553b421fa175db
Paul Kotlyar <unclepaul84@gmail.com>
2017-09-21 11:18:41 -0500

    cleanup server js

--- server.js
+++ server.js
@@ -1,36 +1,25 @@
-var injection = require('@unclepaul/allcountjs');
+const injection = require('@unclepaul/allcountjs');
 const fs = require('fs');
 
-var port = process.env.PORT || 9080;
+process.env['NODE_ENV'] = 'development'; //this is needed because of bug in asset minification. TODO: take out this line when minification bugs are fixed.
 
-process.env['NODE_ENV'] = 'development';
+const port = process.env.PORT || 9080;
+const baseUrl = process.env.BASE_URL || 'http://localhost:' + port;
+const dbUrl = process.env.MONGO_DB_URL;
+
+if (!dbUrl)
+    throw new Error("Must set MONGO_DB_URL env variable!");
+
+const gitRepoUrl = process.env.CONFIG_GIT_URL;
+
+if (!gitRepoUrl)
+    throw new Error("Must set CONFIG_GIT_URL env variable!");
 
 
-injection.bindFactory('defaultBaseUrl', 'http://localhost:' + port);
+injection.bindFactory('defaultBaseUrl', baseUrl);
 injection.bindFactory('port', port);
-injection.bindFactory('dbUrl', 'mongodb://pkallcounttester:UrhmV7OgZrZwNMhm7vGwtG7l9ipRYsPpERgXqCekKo7KfuTQXgvslnm9WU7pi9M8LaejjITNPL4ZWGOZjjZYdg==@pkallcounttester.documents.azure.com:10255/allcount?ssl=true&replicaSet=globaldb');
-injection.bindFactory('gitRepoUrl', 'https://gist.github.com/90492c822dc2d7ed319cdcad9d255de3.git');
-
-
-
-if (process.env.CONFIG_GIT_URL)
-    injection.bindFactory('gitRepoUrl', process.env.CONFIG_GIT_URL);
-
-if (process.env.MONGO_DB_URL)
-    injection.bindFactory('dbUrl', process.env.MONGO_DB_URL);
-
-if (process.env.BASE_URL)
-    injection.bindFactory('defaultBaseUrl', process.env.BASE_URL);
-
-
-//const adalmodule = require("@unclepaul/allcount-adal-auth-module");
-
-//adalmodule.installModule(injection);
-
-//const eventHubModule = require("@unclepaul/allcount-azure-event-hubs-module");
-
-//eventHubModule.installModule(injection);
-
+injection.bindFactory('dbUrl', dbUrl);
+injection.bindFactory('gitRepoUrl', gitRepoUrl);
 
 if (fs.existsSync("package.json")) {
     injection.installModulesFromPackageJson("package.json");
