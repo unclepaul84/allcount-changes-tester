1082985891d835141c6d663d1de65b5f8a55c70b
Paul Kotlyar <unclepaul84@gmail.com>
2017-09-20 21:49:05 -0500

    changed to use modules

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var injection = require('@unclepaul/allcountjs');
+const fs = require('fs');
 
 var port = process.env.PORT || 9080;
 
@@ -6,8 +7,6 @@ process.env['NODE_ENV'] = 'development';
 
 
 injection.bindFactory('defaultBaseUrl', 'http://localhost:' + port);
-
-
 injection.bindFactory('port', port);
 injection.bindFactory('dbUrl', 'mongodb://pkallcounttester:UrhmV7OgZrZwNMhm7vGwtG7l9ipRYsPpERgXqCekKo7KfuTQXgvslnm9WU7pi9M8LaejjITNPL4ZWGOZjjZYdg==@pkallcounttester.documents.azure.com:10255/allcount?ssl=true&replicaSet=globaldb');
 injection.bindFactory('gitRepoUrl', 'https://gist.github.com/90492c822dc2d7ed319cdcad9d255de3.git');
@@ -24,13 +23,18 @@ if (process.env.BASE_URL)
     injection.bindFactory('defaultBaseUrl', process.env.BASE_URL);
 
 
-const adalmodule = require("@unclepaul/allcount-adal-auth-module");
+//const adalmodule = require("@unclepaul/allcount-adal-auth-module");
 
-adalmodule.installModule(injection);
+//adalmodule.installModule(injection);
 
-const eventHubModule = require("./azure-event-hubs-module/azure-event-hubs-module.js");
+//const eventHubModule = require("@unclepaul/allcount-azure-event-hubs-module");
 
-eventHubModule.installModule(injection);
+//eventHubModule.installModule(injection);
+
+
+if (fs.existsSync("package.json")) {
+    injection.installModulesFromPackageJson("package.json");
+}
 
 var server = injection.inject('allcountServerStartup');
 
