80ca5ebcc16f31bc1f0f1f174177e7dd71c33013
Paul Kotlyar <unclepaul84@gmail.com>
2017-09-28 16:08:35 -0500

    change change get token function

--- allcount-modules/allcount-getapikey-module/allcount-getapikey-appConfig.js
+++ allcount-modules/allcount-getapikey-module/allcount-getapikey-appConfig.js
@@ -12,8 +12,15 @@ module.exports = function ( securityService, A) {
                             id: 'getAccessToken',
                             name: "Reveal API Key",
                             perform: function (User, Actions) {
+                                
+                                const crud = Crud.actionContextCrud();
 
-                                return Actions.modalResult('API Key', securityService.generateToken(User));
+                                return crud.readEntity(Actions.selectedEntityId()).then(function (entity) {
+                                
+                                    return Actions.modalResult('Api Key', securityService.generateToken(entity));
+
+                                });
+                                
                             },
                             actionTarget: 'single-item'
                         }
