bfc1b1d1fd02b1ff9cb0962c5c647d2751a79e1c
Paul Kotlyar <unclepaul84@gmail.com>
2017-09-28 16:10:48 -0500

    change change get token function

--- allcount-modules/allcount-getapikey-module/allcount-getapikey-appConfig.js
+++ allcount-modules/allcount-getapikey-module/allcount-getapikey-appConfig.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function ( securityService, A) {
+module.exports = function (securityService, A) {
 
     A.app({
         entities: function (Fields, Crud, Security) {
@@ -12,15 +12,15 @@ module.exports = function ( securityService, A) {
                             id: 'getAccessToken',
                             name: "Reveal API Key",
                             perform: function (User, Actions) {
-                                
+
                                 const crud = Crud.actionContextCrud();
 
                                 return crud.readEntity(Actions.selectedEntityId()).then(function (entity) {
-                                
+
                                     return Actions.modalResult('Api Key', securityService.generateToken(entity));
 
                                 });
-                                
+
                             },
                             actionTarget: 'single-item'
                         }
