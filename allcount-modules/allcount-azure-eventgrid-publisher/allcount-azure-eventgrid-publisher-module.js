587734cd9395a4d54a0eb16dda6a342f8ed5b126
Paul Kotlyar <unclepaul84@gmail.com>
2017-10-05 16:46:22 -0500

    moved to packagte

--- allcount-modules/allcount-azure-eventgrid-publisher/allcount-azure-eventgrid-publisher-module.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-exports.installModule = function (injection) {
-
-    injection.bindMultiple('compileServices', ['allcount-azure-eventgrid-publisher-service']);
-    injection.bindFactory('allcount-azure-eventgrid-publisher-service',  require('./allcount-azure-eventgrid-publisher-service.js')); //inject module
-    
-};
